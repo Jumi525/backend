3b75d8a02c8f3e7aa916d7ff0e9c3aa1673606cb
jumi525 <jumiklein525@gmail.com>
2025-01-07 03:42:34 +0100

    added

--- src/auths.js
+++ src/auths.js
@@ -12,7 +12,8 @@ app.use(cors());
 const SECRET_KEY = "your-secret-key"; // Replace with a secure key
 
 mongoose.connect(
-  "mongodb+srv://jumiklein5255:1jBQQH1maIZnb4fw@cluster0.wkyf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/UserTask"
+  "mongodb+srv://jumiklein5255:1jBQQH1maIZnb4fw@cluster0.wkyf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?retryWrites=true&w=majority&tls=true/UserTask",
+  { tls: true, tlsAllowInvalidCertificates: false }
 );
 
 mongoose.connection.once("open", () => {
