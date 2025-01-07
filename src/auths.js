eddb45e84fdd7d4800012a2bd8a2e7c4381c3d75
jumi525 <jumiklein525@gmail.com>
2025-01-07 03:47:19 +0100

    add

--- src/auths.js
+++ src/auths.js
@@ -12,8 +12,7 @@ app.use(cors());
 const SECRET_KEY = "your-secret-key"; // Replace with a secure key
 
 mongoose.connect(
-  "mongodb+srv://jumiklein5255:1jBQQH1maIZnb4fw@cluster0.wkyf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?retryWrites=true&w=majority&tls=true/UserTask",
-  { tls: true, tlsAllowInvalidCertificates: false }
+  "mongodb+srv://jumiklein5255:1jBQQH1maIZnb4fw@cluster0.wkyf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?retryWrites=true&w=majority&tls=true/UserTask"
 );
 
 mongoose.connection.once("open", () => {
