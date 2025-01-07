3024c57d70847ee0bc8535b7e3480480774a5121
jumi525 <jumiklein525@gmail.com>
2025-01-07 20:07:48 +0100

    changes

--- src/auths.js
+++ src/auths.js
@@ -12,7 +12,7 @@ app.use(cors());
 const SECRET_KEY = "your-secret-key"; // Replace with a secure key
 
 mongoose.connect(
-  "mongodb+srv://jumiklein5255:1jBQQH1maIZnb4fw@cluster0.wkyf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/UserTask",
+  "mongodb+srv://jumiklein5255:1jBQQH1maIZnb4fw@cluster0.wkyf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   { tls: true, tlsAllowInvalidCertificates: false }
 );
 
